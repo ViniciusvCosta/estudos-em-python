1a1cdc54ca581ab0422711274e5a2dd395bd1144
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-01-27 20:56:12 -0300

    0

---
+++ Mundo-1/AULA01/EX03.py
@@ -0,0 +1,4 @@
+dia=input('Dia ')
+mes=input('Mês ')
+ano=input('ano ')
+print('Você nasceu no dia',dia,'de',mes,'do ano de',ano,'correto?')
---
+++ Mundo-1/AULA01/EX04.py
@@ -0,0 +1,3 @@
+num1=int(input('digite o primeiro numero '))
+num2=int(input('digite o segundo numero '))
+print('A soma é:',num1+num2)
