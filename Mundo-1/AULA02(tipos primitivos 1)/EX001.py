a1063895592ab66902d26499319b9ca6d5ef4df2
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-01-28 23:00:22 -0300

    1

---
+++ Mundo-1/AULA02(tipos primitivos 1)/EX001.py
@@ -0,0 +1,6 @@
+#Tipo primitivo "int" para  converter dados para números inteiros.
+
+num1 = int(input('Digite o primeiro valor '))
+num2 = int(input('Digite o segundo valor '))
+s = num1+num2
+print('A soma dos valores é {}'.format(s))
---
+++ Mundo-1/AULA02(tipos primitivos 1)/TESTE01.py
@@ -0,0 +1,6 @@
+#.format
+
+n1 = int(input('Digite um valor '))
+n2 = int(input('Digite Outro valor '))
+s =  n1 + n2
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
