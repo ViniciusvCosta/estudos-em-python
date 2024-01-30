9f8d9b86893f6434a62551f4228bf4e422e12e30
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-01-30 20:49:01 -0300

    TESTe01 operadores logicos1

--- Mundo-1/AULA03 (Operadores aritméticos)/TESTE01.py
+++ Mundo-1/AULA03 (Operadores aritméticos)/TESTE01.py
@@ -1,1 +1,12 @@
- 
+n1 = int(input('Valor = '))
+n2 = int(input('Valor = '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print(' A soma é {}'.format(s))
+print('O produto é {}'.format(m))
+print('A divisão é {:.2f}'.format(d))
+print('A divisão inteira é {}'.format(di))
+print('A potencia é {}'.format(e))
