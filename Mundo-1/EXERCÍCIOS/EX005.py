ce8d3f1861eb2f686c1976811f73433f786f2422
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-01-30 20:57:00 -0300

    EX005

---
+++ Mundo-1/EXERCÍCIOS/EX005.py
@@ -0,0 +1,5 @@
+# Sucessor e antecessor
+n = int(input('Digite um numero '))
+s = n + 1
+d = n - 1
+print('Você digitou {} \n O sucessor desse numero é {} \n O antecessor desse numero é {}'. format(n, s, d))
