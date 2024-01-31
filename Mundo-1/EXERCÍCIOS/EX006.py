3e8fcc92f5fe0441129cecdd1e041b1b8b3ef5e2
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-01-30 21:08:59 -0300

    EX006

---
+++ Mundo-1/EXERCÍCIOS/EX006.py
@@ -0,0 +1,7 @@
+# Dobro, triplo, raiz quadrada
+
+n = int(input('Digite um nuemro '))
+m1 = n * 2
+m2 = n * 3
+r = n ** 0.5 
+print('Você digitou {} \n O dobro desse valor é {} \n O triplo desse valor é {} \n A raiz quadrada desse valor é {:.3f}'.format(n, m1, m2, r))
