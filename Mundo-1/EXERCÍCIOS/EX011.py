8c7b74b35a46fd0673cae226906636bf781e5f79
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-01 20:03:17 -0300

    finaalizado

--- Mundo-1/EXERCÍCIOS/EX011.py
+++ Mundo-1/EXERCÍCIOS/EX011.py
@@ -3,4 +3,5 @@ altu = int(input('Largura: '))
 mq = larg * altu 
 rend = int(input('Redimento do material em m²: '))
 resu = rend * mq
-print('Você ira precisar de ')
+most = resu / rend
+print('Você vai precisar de {} pra a {}m²'.format(most, mq))
