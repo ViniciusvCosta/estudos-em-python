a89a97eb9f3342332a8b3d33662f6910ef791485
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-01-31 21:43:34 -0300

    Comentarios

--- Mundo-1/EXERCÍCIOS/EX010.py
+++ Mundo-1/EXERCÍCIOS/EX010.py
@@ -1,5 +1,11 @@
+#conversor de moeda
+
+#Entrada do valor em real
 R = int(input('Qual é o valor a ser convertido R$:'))
+#Entrada do nome da moeda
 M = (input('Qual é a moeda que deseja fazer a coversão?: '))
+#Entrada do valor atual da moeda
 ME = int(input('Qual é o valor atual dessa moeda? '))
+#Calculo de conversão
 C = R / ME
 print('O valor {} envertido em {} da {}'.format(R, M, C))
