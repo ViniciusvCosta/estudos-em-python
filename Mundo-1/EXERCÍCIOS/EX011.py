dcc54ba5780956d32831d6b99c2a163a136e3af1
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-01-31 21:59:58 -0300

    EX011 incompleto

---
+++ Mundo-1/EXERCÍCIOS/EX011.py
@@ -0,0 +1,6 @@
+larg = int(input('Altura: '))
+altu = int(input('Largura: '))
+mq = larg * altu 
+rend = int(input('Redimento do material em m²: '))
+resu = rend * mq
+print('Você ira precisar de ')
