90a63613f2cfaa6e3b64155eefe8244fd3106209
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-04 11:25:44 -0300

    EX019

---
+++ Mundo-1/EXERCÍCIOS/EX019.py
@@ -0,0 +1,8 @@
+from random import choice
+n1 = input('Nome do primeiro aluno: ')
+n2 = input('Nome do segundo aluno: ')
+n3 = input('Nome do terceiro aluno: ')
+n4 = input('Nome do quarto aluno: ')
+lista =[n1, n2, n3, n4]
+escolhido = choice(lista)
+print('O aluno escolhido foi: {}'.format(escolhido))
