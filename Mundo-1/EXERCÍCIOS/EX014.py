3eea61acabd42ff1378f2ee386ac098811a16ae9
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-04 08:15:00 -0300

    aprimoramento

--- Mundo-1/EXERCÍCIOS/EX014.py
+++ Mundo-1/EXERCÍCIOS/EX014.py
@@ -1,5 +1,5 @@
 # Conversor de temperatura
 
-graC = int(input('Qual é a temperatura em °C: '))
+graC = float(input('Qual é a temperatura em °C: '))
 graF = graC * 1.8 + 32
 print('A temperatura °C:{} Convetida ficar °F:{}'.format(graC, graF))
