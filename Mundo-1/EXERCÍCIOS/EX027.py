62bcdf522be82a7dab8183629b8cb6c3a1e17e70
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-11 06:53:25 -0300

    Update EX027

--- Mundo-1/EXERCÍCIOS/EX027.py
+++ Mundo-1/EXERCÍCIOS/EX027.py
@@ -1,4 +1,4 @@
-nome = input('Qual é o seu  nome: ')
+nome = str(input('Qual é o seu  nome: ')).upper().strip()
 dividido = nome.split()
 print(dividido[0])
-print(dividido[3])
+print(dividido[len(dividido)-1])
