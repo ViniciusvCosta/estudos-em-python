407ae3c956ba7d412c517c6c073e8d39e2791946
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-12 09:53:01 -0300

    teste 01

---
+++ Mundo-1/AULA 06(Condições 01)/teste 01.py
@@ -0,0 +1,7 @@
+tempo = int(input('Qunatoa anos temo seu carro? '))
+#if tempo <=3:
+   # print('Que lindo cara choro choro choro!')
+#else:
+   #print('Mamaco aranha.')
+print('Carro novo'if tempo <=3 else 'carro vallho')
+print('--Isso é tudo pppesoal--')
