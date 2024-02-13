4ff503d9c7d80cc4e0a3ad4cddde72b99b114fc5
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-13 08:08:51 -0300

    Update EX028

--- EXERCÍCIOS/EX028.py
+++ EXERCÍCIOS/EX028.py
@@ -1,6 +1,9 @@
 from random import randint
+from time import sleep
 computador = randint(0,5) #Esse comando faz o computador sortear um nomero aleatoria entre 0 e 5
 jogador = int(input('Em que numero eu pensei? '))
+print('PROCESSANDO...')
+sleep(2)
 if jogador == computador:
     print('EU pensei no numero {}, Você ganhou o jogo'.format(computador))
 else:
