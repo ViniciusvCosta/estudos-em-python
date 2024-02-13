68a271e047de2b49f4ff22abf9e1d40b43446b1b
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-13 08:22:37 -0300

    f

--- EXERCÍCIOS/EX029.py
+++ EXERCÍCIOS/EX029.py
@@ -3,5 +3,4 @@ if velocidade > 80:
     print('MULTADO. Você excedeu o limite permitido que é de 80Km/h')
     multa = (velocidade - 80) * 7
     print('Você recebeu uma multa de R${:.2f}!'.format(multa))
-else:
-    print('Dirija com cuidado, tenha um otimo dia!')
+print('Dirija com cuidado, tenha um otimo dia!')
