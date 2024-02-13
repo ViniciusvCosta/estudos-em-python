665789058fc0434f70c8f2f06cbb9972f408abc7
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-13 08:28:22 -0300

    EX030.py

---
+++ EXERCÍCIOS/EX030.py
@@ -0,0 +1,7 @@
+num = int(input('Digite um numero: '))
+resultado = num % 2
+if resultado == 0:
+    print('O numero {} é par'.format(num))
+else:
+    print('O numero {} é impar'.format(num))
+    
