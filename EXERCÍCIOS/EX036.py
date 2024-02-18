deb5463588edfb79c453061e33721742fc9b5ba1
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-18 09:04:16 -0300

    Teste EX036

--- EXERCÍCIOS/EX036.py
+++ EXERCÍCIOS/EX036.py
@@ -1,3 +1,8 @@
 ValCasa = float(input('Valor da casa: '))
 RendaMen = float(input('Sua renda mensal: '))
 QuaAnos = int(input('Tempo do financiamento: '))
+ValPrest = ValCasa / QuaAnos
+if ValPrest >= 30% RendaMen:
+    print('Seu financiamento foi negado!')
+else:
+    print('Seu financiamento foi aceito!, parabens.')
