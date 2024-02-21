395cb4c8a387cf57d62819d23f8dee566694ca21
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-21 06:55:55 -0300

    update EX039

--- EXERCÍCIOS/Ex039.py
+++ EXERCÍCIOS/Ex039.py
@@ -1,4 +1,4 @@
-import datetime 
+'''import datetime 
 Nasc = int(input('Digite sua data de nascimento: '))
 DataNasc = datetime.datetime.strptime(Nasc, format)
 DataAtual = datetime.today()
@@ -8,4 +8,20 @@ if Idade < 18:
 elif Idade == 18:
     print('Já esta na hora de se alistar!')
 elif Idade > 18:
-    print('Já passou da hora de fazer seu alistamento!')
+    print('Já passou da hora de fazer seu alistamento!')'''
+
+from datetime import date
+AnoAtual = date.today().year
+AnoNasc = int(input('Digite o ano de nascimento'))
+idade = AnoAtual - AnoNasc
+
+if idade == 18:
+    print('Já esta na hora de se alistar!') 
+
+elif idade < 18:
+    saldo = 18 - idade
+    print('Você ainda vai ter que se alistar em {} anos!'. format(saldo))
+
+elif idade > 18:
+    saldo = idade -18
+    print('Voê deveria ter se alistado ha {} anos!'.format(saldo))
