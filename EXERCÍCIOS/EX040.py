126368ce18a308570b9316e248603789e5521cdd
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-22 06:46:05 -0300

    updateEX40

--- EXERCÍCIOS/EX040.py
+++ EXERCÍCIOS/EX040.py
@@ -1,9 +1,9 @@
 nome = str(input('Digite o nome do aluno(a): '))
-n1 = float(input('Digite a nota do PRIMEIRO bimestre: '))
-n2 = float(input('Digite a nota do SEGUNDO bimestre: '))
-n3 = float(input('Digite a nota do TERCEIRO bimestre: '))
-n4 = float(input('Digite a nota do QUARTO bimestre: '))
-media = (n1 + n2 + n3 + n4) / 4
+nota1 = float(input('Digite a nota do PRIMEIRO bimestre: '))
+nota2 = float(input('Digite a nota do SEGUNDO bimestre: '))
+nota3 = float(input('Digite a nota do TERCEIRO bimestre: '))
+nota4 = float(input('Digite a nota do QUARTO bimestre: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
 if media < 5.0:
     print('O anluno(a) {} teve media de {}, portanto foi reprovado!'.format(nome, media))
 elif media >= 5.0 and media <= 6.9:
