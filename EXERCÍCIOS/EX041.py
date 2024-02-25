b7f33cdbb823d7cf7c5062fa0ec05532a2d30e0a
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-24 22:52:45 -0300

    EX041

--- EXERCÍCIOS/EX041.py
+++ EXERCÍCIOS/EX041.py
@@ -1,1 +1,19 @@
+from datetime import date
+atual = date.today().year
+nascimento = int(input('Em qual ano você nascel '))
+idade = atual - nascimento
+print('O atleta tem {}'.format(idade))
+
+if idade <= 9:
+    print('categoria MIRIM')
+elif idade <= 14:
+    print('Categoria INFANTIL')
+elif idade <= 19:
+    print('Categoria JUNIOR')
+elif idade <= 25:
+    print('Categoria SENIOR')
+else:
+    print('Categoria MASTER') 
+
+
 
