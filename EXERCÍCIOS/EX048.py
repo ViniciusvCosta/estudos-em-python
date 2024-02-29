09871b17d63512e4feaa67f89b5d72d83c67f91d
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-02-29 06:42:19 -0300

    1

--- EXERCÍCIOS/EX048.py
+++ EXERCÍCIOS/EX048.py
@@ -1,6 +1,8 @@
 soma = 0
+cont = 0
 for n in range(1 ,501, 2):
     if n % 3 == 0:
+        cont = cont + 1
         soma = soma + n
-print('A soma de todos os valores solicitados é {}'.format(soma))
+print('A soma de todos os {} valores solicitados é {}'.format(cont, soma))
        
