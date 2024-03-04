5fc6eb74d87f415f4f987745c02a6070d14b4256
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-03-04 06:28:32 -0300

    EX052

---
+++ EXERCÍCIOS/EX052.py
@@ -0,0 +1,14 @@
+num = int(input('\033[m Digite um nuemero: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[34m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='' )
+    print('{} '.format(c), end='' )
+print('\n\033[m O numero {} foi divisivel {} vezes'.format(num, tot))
+if tot == 2:
+    print('E por isso ele é PRIMO')
+else:
+    print('E por isso, ele NãO é PRIMO')
