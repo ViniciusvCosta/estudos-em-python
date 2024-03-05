7125e11ffeab1a9d574738acfccd1f19a8accbf4
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-03-05 20:51:54 -0300

    Calculadora de media

---
+++ TESTES/calculadora de media.py
@@ -0,0 +1,8 @@
+print('CALCULADORA DE MEDIA ESCOLAR')
+#entrada de dados (nome, notas bimestrais)
+nome = input('NOME DO ALUNO: ')
+n1 = float(input('Nota primeiro bimestre: '))
+n2 = float(input('Nota segundo bimestre: '))
+n3 = float(input('Nota terceiro bimestre: '))
+n4 = float(input('Nota quarto bimestre: '))
+#calculo de media
