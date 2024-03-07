cf3e03eac42f906e8b407af04e985b3008e5a289
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-03-07 06:38:20 -0300

    EX054

---
+++ EXERCÍCIOS/EX054.py
@@ -0,0 +1,12 @@
+from datetime import date
+atual = date.today().year
+totmaior = 0
+totmenor = 0
+for pess in range(1, 8):
+    nasc = int(input('em que ano a {} pessoa nasceu? '.format(pess)))
+    idade = atual - nasc
+    if idade >= 21:
+        totmaior += 1
+    else: 
+        totmenor += 1
+print('Ao todo tivemos {} pessoas maior e {} pessoas menor. '.format(totmaior, totmenor))
