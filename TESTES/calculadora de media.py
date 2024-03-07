442beeb7476a784fdde58c87427c2c15159574f4
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-03-07 06:46:33 -0300

    Update teste01

--- TESTES/calculadora de media.py
+++ TESTES/calculadora de media.py
@@ -6,3 +6,10 @@ n2 = float(input('Nota segundo bimestre: '))
 n3 = float(input('Nota terceiro bimestre: '))
 n4 = float(input('Nota quarto bimestre: '))
 #calculo de media
+media = (n1 + n2 + n3 + n4) / 4
+if media >= 7:
+    print('O(a) aluno(a) {} teve media de {}, portanto foi aprovado(a)'.format(nome, media))
+elif media >=5:
+     print('O(a) aluno(a) {} teve media de {}, portanto esta em recuparação'.format(nome, media))
+else:
+    print('O(a) aluno(a) {} teve media de {}, portanto foi reprovado(a)'.format(nome, media))
