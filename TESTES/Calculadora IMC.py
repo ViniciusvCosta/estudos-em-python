e329ab16c307792d4aa81c15c1bee174b96a12a1
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-03-16 16:43:57 -0300

    Update IMC

--- TESTES/Calculadora IMC.py
+++ TESTES/Calculadora IMC.py
@@ -1,18 +1,23 @@
+continuar = 'S'
 print('Calculadora de indece de massa corporal (I.M.C)')
 nome = str(input('Digite seu nome: '))
-altura = float(input('Digite a sua altura: '))
-peso = float(input('Digite seu peso: '))
-imc = peso/ (altura **2)
+while continuar not in 'N':
+    altura = float(input('Digite a sua altura: '))
+    peso = float(input('Digite seu peso: '))
+    imc = peso/ (altura **2)
 
-if imc <= 18.5:
-    print('{} seu IMC é de {:.2f}, você esta classificado como ABAIXO DO PESO'.format(nome, imc))
-elif imc <= 24.9:
-    print('{} seu IMC é de {:.2f}, você esta classificado como PESO NORMAL'.format(nome, imc))
-elif imc <= 29.9:
-    print('{} seu IMC é de {:.2f}, você esta classificado como SOBREPESO'.format(nome, imc))
-elif imc <= 34.9:
-    print('{} seu IMC é de {:.2f}, você esta classificado como OBESIDADE'.format(nome, imc))
-elif imc <= 39.9:
-    print('{} seu IMC é de {:.2f}, você esta classificado como OBSIDADE GRAU II'.format(nome, imc))
-else:
-    print('{} seu IMC é de {:.2f}, você esta classificado como OBSIDADE MÓRBIDA'.format(nome, imc))
+    if imc <= 18.5:
+        print('{} seu IMC é de {:.2f}, você esta classificado como ABAIXO DO PESO'.format(nome, imc))
+    elif imc <= 24.9:
+        print('{} seu IMC é de {:.2f}, você esta classificado como PESO NORMAL'.format(nome, imc))
+    elif imc <= 29.9:
+        print('{} seu IMC é de {:.2f}, você esta classificado como SOBREPESO'.format(nome, imc))
+    elif imc <= 34.9:
+        print('{} seu IMC é de {:.2f}, você esta classificado como OBESIDADE'.format(nome, imc))
+    elif imc <= 39.9:
+        print('{} seu IMC é de {:.2f}, você esta classificado como OBSIDADE GRAU II'.format(nome, imc))
+    else:
+        print('{} seu IMC é de {:.2f}, você esta classificado como OBSIDADE MÓRBIDA'.format(nome, imc))
+    
+    continuar = str(input('Fazer novo teste [S/N]: ')).upper().strip()
+print('Fim de execusão')
