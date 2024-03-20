3a97275d35251e4ddfff0c984072b67769a7cf60
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-03-20 06:55:05 -0300

    EX065.py

---
+++ EXERCÍCIOS/EX065(maior e menor).py
@@ -0,0 +1,17 @@
+resp = 's'
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um numero: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num 
+    else:
+        if num > maior:
+            maior = num 
+        elif num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quant
+print('Você digitou {} números, e a média foi {}'.format(quant, media))
+print('O maior valor foi {} e o menor foi {}'.format(maior, menor))
