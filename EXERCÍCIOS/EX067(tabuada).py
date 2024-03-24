0cbda1dae85d9bed046d2f6d9391180adf21a54d
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-03-24 11:40:01 -0300

    1

--- EXERCÍCIOS/EX067(tabuada).py
+++ EXERCÍCIOS/EX067(tabuada).py
@@ -1,1 +1,8 @@
 print('=' * 15, 'TABUDA', '=' * 15)
+n = 0
+while True:
+    n = int(input('Qual tabuada você quer ver? '))
+    if n== 999:
+        break
+    print(n*1, n*2, n*3, n*4, n*5, n*6, n*7, n*7, n*8, n*9, n*10)
+print('=' * 15, 'FIM', '=' * 15)
