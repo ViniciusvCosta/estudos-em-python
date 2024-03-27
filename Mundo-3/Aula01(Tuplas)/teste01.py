1985b482f5a8ce23a21590996d37341aa4548b2c
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-03-27 06:52:30 -0300

    Tuplas

---
+++ Mundo-3/Aula01(Tuplas)/teste01.py
@@ -0,0 +1,8 @@
+lanche = 'Hamburguer', 'Suco', 'Pizza', 'Pudim'
+for cont in range(0, len (lanche)):
+    print(f'Eu vou comer {lanche[cont]}')
+print('Eu comi pra caramba')
+
+for comida in lanche:
+    print('Eu vou comer {}'.format(lanche))
+print('Eu comi pra caramba')
---
+++ Mundo-3/Aula01(Tuplas)/teste02.py
@@ -0,0 +1,5 @@
+a = 2, 5, 4
+b= 5, 8, 1, 2
+c = a + b
+print(c)
+print(c.index(5, 1))
