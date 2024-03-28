fdf1cd0be341b7306c826b36491d0ca5cdc72fb3
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-03-28 20:15:35 -0300

    update EX072

--- EXERCÍCIOS/EX072(Numero por extenso).py
+++ EXERCÍCIOS/EX072(Numero por extenso).py
@@ -1,11 +1,15 @@
+resp = 'S'
 cont = ('zero', 'um', 'dois', 'tres', 'quatro',
          'cinco', 'seis','sete', 'oito', 'nove', 
          'dez', 'onze', 'doze', 'treze', 'quartorze', 
          'quinze', 'dezesseis', 'dezessete', 'dezoito', 
          'dezenove', 'vinte')
-while True:
-    num = int(input('Digite um numero entre 0 e 20: '))
-    if 0 <= num <= 20:
-        break
-    print('Tente novamente. ', end='')
-print('Você digitou o numero {}'.format(cont[num]))
+while resp not in 'N':
+    while True:
+        num = int(input('Digite um numero entre 0 e 20: '))
+        if 0 <= num <= 20:
+            break
+        print('Tente novamente. ', end='')
+    print('Você digitou o numero {}'.format(cont[num]))
+    resp = str(input('Quer continuar [S/N]: ')).upper().strip()     
+print('Fim de programa!')
