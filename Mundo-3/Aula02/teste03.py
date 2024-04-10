fad39d5ed04ac864e71e9fe25b332bfa31e64ee9
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-04-10 06:32:27 -0300

    1

---
+++ Mundo-3/Aula02/teste03.py
@@ -0,0 +1,6 @@
+valores = list()
+for cont in range(0,5):
+    valores.append(int(input('Difite um valor: ')))
+for c, v in enumerate(valores):
+    print(f'Na posição {c} encontrei o valor {v}!')
+print('Cheguei ao final da lista.')
