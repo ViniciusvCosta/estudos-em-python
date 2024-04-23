f5f6e176f207c7fea562c266e415b5f5e114af29
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-04-23 20:00:51 -0300

    Eu sou um indiota

---
+++ Mundo-2/AULA 1(Condiçoes 2)/teste02.py
@@ -0,0 +1,13 @@
+pé = str(input('Você tem pé de porco? [S/N] '))
+if pé in 'Ss':
+    fucinho = str(input('Você tem fucinho de porco? [S/N] '))
+    if fucinho in 'Ss':
+        orelha = str(input('E você tem orelha de porco? [S/N] '))
+        if orelha in 'Ss':
+            print('Então você é um porco hahahahaha')
+        else:
+            print('Que pena!')
+    else:
+        print('Então ta!')
+else:
+    print('ok, passar bem!')
