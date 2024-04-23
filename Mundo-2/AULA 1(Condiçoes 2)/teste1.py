79fbda25c2ec2ac027394d40779dc2489be1c85a
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-04-23 19:52:11 -0300

    0

--- Mundo-2/AULA 1(Condiçoes 2)/teste1.py
+++ Mundo-2/AULA 1(Condiçoes 2)/teste1.py
@@ -1,10 +1,11 @@
-nome = str(input('Qual é o seu nome? ')).strip().lower()
-if nome == 'vinicius':
-    print('Que nome lindo caraa!')
-elif nome == 'pedro' or nome == 'maria' or nome =='paulo':
-    print('Seu nome é bme comun aqui no brasil')
-elif nome == 'mamaco aranha':
-    print('É hora do heroi (musica foda)')
-else:
-    print('Seu nome é normal')
-print('Prazer em te conhecer {}'.format(nome))
+while True:
+    nome = str(input('Qual é o seu nome? ')).strip().lower()
+    if nome == 'vinicius':
+        print('Que nome lindo caraa!')
+    elif nome == 'pedro' or nome == 'maria' or nome =='paulo':
+        print('Seu nome é bem comun aqui no brasil')
+    
+    elif nome in 'mamaco aranha':
+        print('É hora do heroi (musica foda)')
+        break
+    print('Prazer em te conhecer {}'.format(nome))
