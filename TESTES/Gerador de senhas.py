90dd02d56cb08e08f92513be52cc9efdcdd411d4
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-04-27 20:10:44 -0300

    Gerador de snhas.py

--- TESTES/Gerador de senhas.py
+++ TESTES/Gerador de senhas.py
@@ -1,3 +1,22 @@
-
-print('Gerador de senhas')
-print('-=' * 10)
+import random
+num = str(random.randint(1048698735, 9187642503))
+while True:                         
+    senha =''
+    for numero in num:
+        if numero in '0': senha += 'A'
+        elif numero in '1': senha += '5'
+        elif numero in '2': senha += 'q' 
+        elif numero in '3': senha += '@'
+        elif numero in '4': senha +='k'
+        elif numero in '5': senha += 'H'
+        elif numero in '6': senha += '9'
+        elif numero in '7': senha += '&'
+        elif numero in '8': senha += '#'
+        elif numero in '9': senha += '?'
+        
+    print('-=' *30)
+    print(f'Senha: {senha}')
+    print('-=' *30)
+    r = str(input('Outra? [S/N] '))
+    if r in 'Nn':
+        break
