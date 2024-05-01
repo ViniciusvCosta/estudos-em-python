0cab39758d84937e0acecdd4f70d4896b77e8097
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-05-01 18:13:02 -0300

    teste 05

--- Mundo-2/AULA02(Estrutura de repetição for)/teste01.py
+++ Mundo-2/AULA02(Estrutura de repetição for)/teste01.py
@@ -1,5 +1,5 @@
 #Estrutura de repetiçao FOR
 s = int(input('Digite um numero '))
-for c in range(0, s):
+for c in range(0, s+1):
     print(c)
 print('FIM')
---
+++ Mundo-2/AULA02(Estrutura de repetição for)/teste05.py
@@ -0,0 +1,9 @@
+# Onde vai começar
+i = int(input('Início: '))
+# Onde vai terminar
+f = int(input('Fim: '))
+# De quanto em quanto vai pular
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
