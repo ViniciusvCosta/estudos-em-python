f20e85dbfd20ef6e6494d33f18c91d5f890144d0
ViniciusvCosta <viniciusvcosta1928@gmail.com>
2024-05-02 19:50:24 -0300

    0

--- EXERCÍCIOS/EX051(Progressão aritmética).py
+++ EXERCÍCIOS/EX051(Progressão aritmética).py
@@ -2,5 +2,5 @@ primeiro = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 decimo = primeiro + (10 - 1) * razao
 for c in range(primeiro, decimo + razao, razao):
-    print('{}'.format(c), end='-> ')
+    print(f'{c}', end='-> ')
 print('ACABOU')
